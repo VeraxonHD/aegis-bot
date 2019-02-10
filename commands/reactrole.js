3bda60e1067a6fe17e194331394d0c3758f0fa71
VeraxonHD <toxicscopesgames@gmail.com>
2019-02-10 12:41:00 +0000

    Deletion v0.1 indev

--- commands/reactrole.js
+++ commands/reactrole.js
@@ -118,6 +118,44 @@ module.exports = {
                     }
                 }
             }
+        }else if(args[0] == "delete"){
+            var messageUnique = args[1];
+            var emojistring = args[2];
+
+            if(!reactroles[messageUnique]){
+                message.reply("That message unique does not exist. Please try another.");
+            }else{
+                try{
+                    emojistring = emojistring.split(":")[1];
+                    var emoji = client.emojis.find(val => val.name === emojistring);
+                    if(reactroles[messageUnique][emoji.id]){
+                        var reactrolemessage = message.guild.channels.get(reactroles[messageUnique].channelid).fetchMessage(reactroles[messageUnique].messageid).then(msg =>{
+                            msg.reactions.forEach(reaction => {
+                                if(reaction.emoji.id == emoji.id){
+                                    reaction.remove();
+
+                                    jsonfile.writeFile("./reactroles.json", reactroles, {spaces: 4}, err =>{
+                                        //If a success, end of module. Send the success to the user.
+                                        if(!err){
+                                            delete reactroles[messageUnique][emoji.id];
+                                            return message.reply(`Success! You have removed **${emoji}** from message **${messageUnique}**`);
+                                        //If not, send the user an error message and print the error details to console for analysis.
+                                        }else{
+                                            console.log(err)
+                                            return message.reply("There was an error in this request, due to a failure to write to file. Please try again later.");
+                                        }
+                                    });
+                                }
+                            });
+                        });
+                    }else{
+                        message.reply("That emoji is not a member of that messageUnique. Try again with a different emoji or messageUnique.");
+                    }
+                }catch(err){
+                    message.reply("That is not a valid emoji. Please try again with a different emoji.");
+                    console.log(err);
+                }
+            }
         }
     }
 };
