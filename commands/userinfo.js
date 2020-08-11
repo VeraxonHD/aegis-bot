b5e2a543983388a170bf9b3cd63a8c140a0940b8
Finley Knapman <f.g.knapman@student.reading.ac.uk>
2020-08-11 13:55:22 +0100

    Re-designed userinfo to include new saved data

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -10,6 +10,8 @@ module.exports = {
             return errLib.invalidPermissions(message.channel, "userinfo", "MANAGE_MESSAGES");
         }
         var mainfile = require("../aegis.js");
+        var GuildDB = mainfile.sendGuildDB();
+        var UserDB = mainfile.sendUserDB();
         var df = require("dateformat")
         var Discord = require("discord.js")
 
@@ -18,35 +20,40 @@ module.exports = {
         }
         var member;
         var snowflakeRegexTest = new RegExp("([0-9]{18})");
-            if(args[0].length == 18 && snowflakeRegexTest.test(args[0])){
-                member = message.guild.members.cache.get(args[0]);
-            }else if(message.mentions.members.first()){
-                member = message.mentions.members.first();
-            }else{
-                return errLib.userNotFound(message.channel, args[0]);
-            }
+        if(args[0].length == 18 && snowflakeRegexTest.test(args[0])){
+            member = message.guild.members.cache.get(args[0]);
+        }else if(message.mentions.members.first()){
+            member = message.mentions.members.first();
+        }else{
+            return errLib.userNotFound(message.channel, args[0]);
+        }
 
-        var database = mainfile.sendDB();
-        database.findOne({where:{userid: member.id}}).then(row => {
-            var data = {
-                "globalMessageCount": row.globalMessageCount,
-                "lsChannel": row.lastSeenChan,
-                "lsGuild": row.lastSeenGuild,
-                "lsTime": row.lastSeenTS,
-                "globalWarnings": row.globalWarnings,
-                "accCreation": row.accCreationTS,
-                "userid": row.userid
+        
+        GuildDB.findOne({where:{guildid: message.guild.id}}).then(guildData =>{
+            var guildMemberData = guildData.members[member.id]
+            var guildData = {
+                "guildJoinDateTime": guildMemberData.joinDateTime,
+                "guildMessageCount": guildMemberData.messageCount,
+                "guildWarnings": guildMemberData.warnings
             }
-            const embed = new Discord.MessageEmbed()
-                .addField("UserID", data.userid)
-                .addField("Message Count", data.globalMessageCount)
-                .addField("Warning Count", data.globalWarnings)
-                .addField("Last Seen", `At: ${df(data.lsTime, "dd/mm/yyyy, HH:MM:ss")}\nIn: ${data.lsGuild} (#${data.lsChannel})`)
-                .addField("Joined The Guild", df(member.joinedTimestamp, "dd/mm/yyyy, HH:MM:ss"))
-                .setColor("#00C597")
-                .setFooter("AEGIS-USERINFO Command")
-                .setTimestamp(new Date())
-            message.channel.send(`Userinfo for **${args[0]}**`, {embed})
+            UserDB.findOne({where:{userid: member.id}}).then(userMemberData => {
+                var userData = {
+                    "globalMessageCount": userMemberData.globalMessageCount,
+                    "lsChannel": userMemberData.lastSeenChan,
+                    "lsGuild": userMemberData.lastSeenGuild,
+                    "lsTime": userMemberData.lastSeenTS,
+                    "globalWarnings": userMemberData.globalWarnings,
+                }
+                const embed = new Discord.MessageEmbed()
+                    .setAuthor(`User Data for ${member.user.tag}`)
+                    .addField("General User Data", `**User ID**: ${member.id}\n**Account Tag**: ${member.user.tag}\n**Is a Bot?**: ${member.user.bot}\n**Created At**: ${member.user.createdAt}`)
+                    .addField("Global Aegis Data", `**Messages Sent**: ${userData.globalMessageCount}\n**Last Seen** in ${userData.lsGuild} (#${userData.lsChan}) at ${userData.lsTime}\n**Global Warning Count**: ${userData.globalWarnings}`)
+                    .addField("Guild-specific Aegis Data", `**Joined Guild At**: ${guildData.guildJoinDateTime}\n**Messages sent in server**: ${guildData.guildMessageCount}\n**Warnings in server**: ${guildData.guildWarnings}`)
+                    .setColor("#00C597")
+                    .setFooter("AEGIS-USERINFO Command")
+                    .setTimestamp(new Date())
+                message.channel.send(`Userinfo for **${member}**`, {embed})
+            })
         })
     }
 };
